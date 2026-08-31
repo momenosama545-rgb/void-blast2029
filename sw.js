d373a5120765e373a202f5191e55d1977cc28bca
momenosama545-rgb <momenosama545@gmail.com>
2026-08-31 23:57:56 +0300

    10.10.1

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'void-blast-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
